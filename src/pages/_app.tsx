0471dfd2b42e406bc004cb38ed25c235414cbee2
Mateusz Szczepański <mattszczp@gmail.com>
2022-08-18 21:42:52 +0200

    fix, chore: fix a typo in environment variable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,8 +19,10 @@ const MyApp: AppType = ({
 
 const getBaseUrl = () => {
   if (typeof window !== undefined) return ""; // browser should use relative url
-  if (process.env.RAILWAY_STATIC_URL)
-    return `https://${process.env.RAILWAY_STATUC_URL}`; // SSR should use vercel url
+  if (process.env.RAILWAY_STATIC_URL) {
+    console.log(`${process.env.RAILWAY_STATIC_URL}`);
+    return `https://${process.env.RAILWAY_STATIC_URL}`; // SSR should use vercel url
+  }
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
